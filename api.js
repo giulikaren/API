b76c28157f813046aa918b7ce1ef747973967475
Karen <giulianaferreira2008@gmail.com>
2023-09-03 13:46:11 -0300

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,17 @@
-fetch(URL)
-.then(response => response.JSON())
-.then(data => {
-    console.log( "Datos de la api",data)
-})
+
+
+
+const key = "uVKWQe5UlFqjSPMOKqjHwos2cYDgGHSJKakXuszy";
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${key}`;
+
+fetch(apiUrl)
+  .then(response => response.json())
+  .then(data => {
+    console.log("Datos de la API:", data);
+  })
+  .catch(error => {
+    console.error("Error al realizar la solicitud:", error);
+  });
+
+
+
